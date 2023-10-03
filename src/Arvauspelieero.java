00501dac4c30eb1f3b8d186b0dd788585bb07cfa
Eero Pankka <eero23005@student.hamk.fi>
2023-10-03 11:59:43 +0300

    Initial commit

---
+++ src/Arvauspelieero.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Arvauspelieero {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int guessCount = 0;
+
+        System.out.println("Guess my name (type stop to exit)");
+        String guess;
+
+        do {
+            guess = in.nextLine();
+            
+            String excludedWord = "stop";
+                       
+
+            if (guess.equalsIgnoreCase(excludedWord)) {
+                
+                break;
+                
+            } else {
+            	
+            guessCount++;
+                
+            String correctName = "Emma";
+            
+           } if(guess.equalsIgnoreCase("Emma")) {
+                System.out.println("Congratulations!");
+                break;
+            }
+
+              else {
+
+        	System.out.println("Guess my name (type stop to exit)");
+           }
+        }while(true);
+        
+        System.out.println("You guessed " + guessCount + " times.");
+        System.out.println("Thank you for playing!");
+}}
