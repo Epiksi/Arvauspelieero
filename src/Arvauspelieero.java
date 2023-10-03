c9d22bab4b4cf1affc6289e602eacd731f470ba4
Eero Pankka <eero23005@student.hamk.fi>
2023-10-03 12:07:55 +0300

    Koodin ulkoasu
    Koodi "siistitty" tasaiseksi

--- src/Arvauspelieero.java
+++ src/Arvauspelieero.java
@@ -2,40 +2,45 @@ import java.util.Scanner;
 
 public class Arvauspelieero {
     public static void main(String[] args) {
+    	
     	/// Scanneri käyttäjänsyötettä varten.
-        Scanner in = new Scanner(System.in);
-        int guessCount = 0;
+    	
+Scanner in = new Scanner(System.in);
         
-        System.out.println("Guess my name (type stop to exit)");
-        String guess;
+int guessCount = 0;
+        
+System.out.println("Guess my name (type stop to exit)");
+        
+String guess;
 
-        do {
-            guess = in.nextLine();
+do {
+guess = in.nextLine();
             
-            String excludedWord = "stop";
-                       
+String excludedWord = "stop";                      
 
-            if (guess.equalsIgnoreCase(excludedWord)) {
+if (guess.equalsIgnoreCase(excludedWord)) {
                 
-                break;
+break;
                 
-            } else {
+} else {
             	
-            guessCount++;
+guessCount++;
                 
-            String correctName = "Emma";
+String correctName = "Emma";
             
-           } if(guess.equalsIgnoreCase("Emma")) {
-                System.out.println("Congratulations!");
-                break;
-            }
+} if(guess.equalsIgnoreCase("Emma")) {
+System.out.println("Congratulations!");
 
-              else {
+break;
+}
 
-        	System.out.println("Guess my name (type stop to exit)");
-           }
-        }while(true);
+else {
+
+System.out.println("Guess my name (type stop to exit)");
+}
+}while(true);
         
-        System.out.println("You guessed " + guessCount + " times.");
-        System.out.println("Thank you for playing! Please come again!");
+System.out.println("You guessed " + guessCount + " times.");
+
+System.out.println("Thank you for playing! Please come again!");
 }}
